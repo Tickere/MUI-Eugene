3bb0ce679cb47f8dd83fc8eb5be724411967e054
Tickere <pablo.blumer@gmail.com>
2025-10-01 14:13:18 +0200

    Initial Commit

---
+++ MUI-Eugene/MUI_EugeneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUI_EugeneApp.swift
+//  MUI-Eugene
+//
+//  Created by Pablo Blumer on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MUI_EugeneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MUI-EugeneTests/MUI_EugeneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MUI_EugeneTests.swift
+//  MUI-EugeneTests
+//
+//  Created by Pablo Blumer on 01.10.2025.
+//
+
+import Testing
+@testable import MUI_Eugene
+
+struct MUI_EugeneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
